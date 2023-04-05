635eff6107c8995fc2bceb187daa6f101a207a5f
hiroto-hara <xre0605@gmail.com>
2023-04-05 23:27:46 +0900

    complete basic functions

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -9,8 +9,7 @@ class Admin::UsersController < ApplicationController
       redirect_to admin_user_url(@user), notice: "ユーザー「#{@user.name}」を登録しました。"
     else
       render :new
-  end
-
+    end
   end
 
   def edit
@@ -44,5 +43,9 @@ class Admin::UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+  def require_admin
+    redirect_to root_url unless current_user.admin?
   end
 end
